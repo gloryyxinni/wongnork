69ad837eb167c87d9d37089970a31bc5a623425d
Nattanicha Ramphan <nattanicha.ram@dome.tu.ac.th>
2021-02-02 17:29:15 +0700

    update view welcome: wongnork

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,6 +22,10 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+            <h1 style="color:tomato;color-background:gray;font-style:normal;font-size:70px;">WONGNORK</h1>
+        </div>
+
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
