46c1dd21e280dae99aecc66222f16a59080de49c
Piyanon <60887684+piyanon23@users.noreply.github.com>
2021-02-14 20:24:40 +0700

    create model&migrate: Rating

---
+++ database/migrations/2021_02_13_174615_create_rating_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rating', function (Blueprint $table) {
+            $table->id();
+            $table->float('Food_score', 5, 2);
+            $table->float('Location_score', 5, 2);
+            $table->float('Service_score', 5, 2);
+            $table->float('Total_score', 5, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rating');
+    }
+}
