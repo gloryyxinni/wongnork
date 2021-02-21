631d4fcbc4a53df537f13edaae89a90387f5a8dc
Nattanicha Ramphan <nattanicha.ram@dome.tu.ac.th>
2021-02-21 16:59:33 +0700

    remake UserController(for resource crud)+show user list for admin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,17 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Display a listing of the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function index()
+    {
+        $users = User::first()->paginate(5);
+        return view('admin.userForAdmin', compact('users'))
+        ->with('i', (request()->input('page',1) - 1) * 5);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
     {
         //
     }
+
+  
 }
--- resources/views/admin/userForAdmin.blade.php
+++ resources/views/admin/userForAdmin.blade.php
@@ -1,1 +1,32 @@
-users view
+@extends('admin.layout')
+ 
+@section('content')
+   <h1>User List</h1>
+   <br>
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Password</th>
+        </tr>
+        @foreach ($users as $user)
+        <tr>
+            <td>{{ $user->id }}</td>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>{{ $user->password }}</td>
+            
+        </tr>
+        @endforeach
+    </table>
+    <br>
+    {!! $users->links() !!}
+      
+@endsection
