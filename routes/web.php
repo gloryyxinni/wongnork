99659ee5fd3930d59ad26718cc21b5496a986a7e
Piyanon <60887684+piyanon23@users.noreply.github.com>
2021-05-11 22:18:05 +0700

    update Route
    add route admin / route user

--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,9 @@ use App\Http\Controllers\ReviewController;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
-
-Route::resource('/userForAdmin', UserController::class);
-
-Route::resource('/restaurants', RestaurantController::class);
-Route::resource('/restaurantForAdmin', RestaurantController::class);
-
-Route::resource('/reviews', ReviewController::class);
-Route::resource('/reviewForAdmin', ReviewController::class);
-
-
 
 Route::middleware(['auth:sanctum', 'verified'])->get('user/dashboard', function () {
     return view('dashboard');
@@ -39,6 +29,51 @@ Route::middleware(['auth:sanctum', 'verified'])->get('user/dashboard', function 
 //     return view('admin.RestaurantForAdmin');
 // })->name('admin.dashboard');
 
+
 Route::group(['middleware' => ['auth:sanctum', 'verified', 'authadmin']], function () {
-    Route::get('admin/dashboard', [RestaurantController::class,'index'])->name('admin.dashboard');
+
+    Route::redirect('/', 'admin/restaurantForAdmin');
+    
+    //  Restaurant List
+
+    // Dashboard.
+    Route::get('admin/restaurantForAdmin', [RestaurantController::class,'index'])->name('admin.dashboard');
+
+    // Create 
+    Route::get('admin/create', [RestaurantController::class,'create'])->name('admin.Create');
+
+    // Update
+    Route::get('admin/edit', [RestaurantController::class, 'edit'])->name('admin.edit');
+
+    //Delete
+    Route::get('delete/{id}',[RestaurantController::class, 'destroy'])->name('admin.destroy');
+
+    //Show
+    Route::get('admin/show',[RestaurantController::class, 'show'])->name('admin.show');
+
+    //Route::resource('/userForAdmin', UserController::class);
+    Route::resource('/restaurants', RestaurantController::class);
+    Route::resource('/restaurantForAdmin', RestaurantController::class);
+    
+    // Review List
+    Route::get('admin/reviewForAdmin',[ReviewController::class,'index'])->name('admin.reviewForAdmin');
+    Route::get('DeleteReview/{id}',[ReviewController::class, 'destroy'])->name('reviews.destroy');
+    Route::resource('/reviewForAdmin', ReviewController::class);
+    Route::resource('/review', ReviewController::class);
+    
+    // User List
+    Route::get('admin/userForAdmin',[UserController::class,'index'])->name('admin.userForAdmin');
+    
+
+});
+
+
+Route::group(['middleware' => ['auth:sanctum', 'verified', 'authadmin']], function () {
+
+    Route::redirect('/', 'admin/restaurantForAdmin');
+    
+    //  Restaurant List
+
+    // Dashboard.
+    Route::get('admin/restaurantForAdmin', [RestaurantController::class,'index'])->name('admin.dashboard');
 });
