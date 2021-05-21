94b5c60d41b0fbc21a21c413674df9d00e8aa248
Piyanon <60887684+piyanon23@users.noreply.github.com>
2021-05-21 11:15:09 +0700

    showRestaurantForUser : create UI
    -create HTML showRestaurantForUser
    -create CSS app.blade.php

--- resources/views/user/showRestaurantForUser.blade.php
+++ resources/views/user/showRestaurantForUser.blade.php
@@ -5,53 +5,44 @@
       </h2>
   </x-slot>
   <br>
-@section('content')
 
-
-<div class="row mb-3">
-@foreach ($restaurants as $restaurant)
-<div class="col-4 mt-3">
-  <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="/image/12.jpg" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title" >{{$restaurant->name}}</h5>
-        
-
-        {{-- <ul class="list-group list-group-flush">
-          <li class="list-group-item">{{$restaurant->location}}</li>
-          <li class="list-group-item">{{$restaurant->open_close_time}}</li>
-          <li class="list-group-item">{{$restaurant->phonenumber}}</li>
-        </ul> 
-         --}}
-         
-          <div class="d-flex ">
-          <div class="content text-center">
-              <div class="ratings"> <span class="product-rating">4.6</span><span>/5</span>
-                  <div class="stars"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> </div>
-                  <div class="rating-text"> <span>46 ratings & 15 reviews</span> </div>
-                  
-                  
-              </div>
-              <br>
-              <a href="{{route("ReviewForUser",$restaurant->id)}}" type="submit" class="btn btn-primary" >Reviews</a>
-          </div>
+<div class="container">
+    <div class="row clearfix">
+      @foreach ($restaurants as $restaurant)
+        <div class="col-lg-3 col-md-4 col-sm-12">
+            <div class="card product_item">
+                <div class="body">
+                    <div class="cp_img">
+                        <img src="/image/112.jpg" alt="Product" class="img-fluid">
+                        <div class="hover">
+                            <a href="{{route("ReviewForUser.show",$restaurant->id)}}" class="btn btn-primary btn-sm waves-effect">Review</i></a>
+                            {{-- <a href="javascript:void(0);" class="btn btn-primary btn-sm waves-effect"><i class="zmdi zmdi-shopping-cart"></i></a> --}}
+                        </div>
+                    </div>
+                    <div class="product_details">
+                        <h5><a href="ec-product-detail.html">{{$restaurant->name}}</a></h5>
+                        <ul class="product_price list-unstyled">
+                            <li class="old_price">$16.00</li>
+                            <li class="new_price">$13.00</li>
+                       
+                              <div class="ratings"> <span class="product-rating">4.6</span><span>/5</span>
+                                  <div class="stars"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> </div>
+                                  <div class="rating-text"> <span>46 ratings & 15 reviews</span> </div>
+                              </div> 
+                        </ul>
+                    </div>
+                   
+                </div>
+            </div>
         </div>
-        
-       <br>
-      
-       
-      
-        
-      </div>
-    </div>
-  </div> 
-   
-  
-
-
-  @endforeach
+        @endforeach
   </div>
 </div>
+
+
+
+
+
   
-  @endsection
+ 
 </x-app-layout>
